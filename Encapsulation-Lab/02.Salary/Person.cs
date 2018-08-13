687e9ac3e23319dd9e3b9603b889e231032c7384
Kalina Milanova <milanova1203@gmail.com>
2018-08-13 20:29:15 +0300

    Add files via upload

---
+++ Encapsulation-Lab/02.Salary/Person.cs
@@ -0,0 +1,62 @@
+﻿namespace _02.Salary
+{
+    public class Person
+    {
+        private string firstName;
+
+        private string lastName;
+
+        private int age;
+
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        public string FirstName
+        {
+            get => this.firstName;
+            set => firstName = value;
+        }
+
+        public string LastName
+        {
+            get => this.lastName;
+            set => lastName = value;
+        }
+
+        public int Age
+        {
+            get => this.age;
+            set => age = value;
+        }
+
+        public decimal Salary
+        {
+            get => this.salary;
+            set => salary = value;
+        }
+
+        public void IncreaseSalary(decimal bonus)
+        {
+            if (this.age > 30)
+            {
+                this.salary += this.salary * bonus / 100;
+            }
+            else
+            {
+                this.salary += this.salary * bonus / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.salary:f2} leva.";
+        }
+    }
+}
