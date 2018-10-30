9613a22f1dcf26d28c9cc5d58b90411e4293263d
Kalina <milanova1203@gmail.com>
2018-10-30 12:46:20 +0200

    add exercises from encapsulation

---
+++ Encapsulation-Exercises/ClassBox/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _FootballTeamGenerator
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(length, width, height);
+
+            Console.WriteLine(box.GetSurfaceArea());
+            Console.WriteLine(box.GetLateralSurfaceArea());
+            Console.WriteLine(box.GetVolume());
+        }
+    }
+}
---
+++ Encapsulation-Exercises/ClassBoxDataValidation/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace _FootballTeamGenerator
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine(box.GetSurfaceArea());
+                Console.WriteLine(box.GetLateralSurfaceArea());
+                Console.WriteLine(box.GetVolume());
+            }
+            catch (ArgumentException argumentException)
+            {
+                Console.WriteLine(argumentException.Message);
+            }
+        }
+    }
+}
+
