29d5dd2ba47af4180c7cb67e7acfcc1440c440b1
Kalina <milanova1203@gmail.com>
2018-11-02 17:34:36 +0200

    add

---
+++ InterfacesAndAbstraction-Exercises/01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,14 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        public string Name { get; }
+        public int Age { get; }
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ InterfacesAndAbstraction-Exercises/02.MultipleImplementation/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02.MultipleImplementation
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
